b47e663471fac64ce453b046173b2954faf7e5b4
username5012 <any9900@gmail.com>
2022-12-07 16:47:16 +0900

    crawling 작업 2022

--- job01_crawling.py
+++ job01_crawling.py
@@ -9,7 +9,67 @@ options.add_argument('lang=ko_KR')
 driver = webdriver.Chrome('./chromedriver.exe', options=options)
 
 # years = ['2016', '2017', '2018', '2019', '2020', '2021', '2022']
-url = 'https://movie.naver.com/movie/sdb/browsing/bmovie.naver?open=2022&page=1'
+your_year = 2022
+
+review_num_path = '//*[@id="reviewTab"]/div/div/div[2]/span/em'
+review_button_xpath = '//*[@id = "movieEndTabMenu"]/li[6]/a'
+review_xpath = '//*[@id="content"]/div[1]/div[4]/div[1]/div[4]'
 
 
+for page in range(1,32):
+    url = 'https://movie.naver.com/movie/sdb/browsing/bmovie.naver?open={}&page={}'.format(your_year, page)
+    titles = []
+    reviews = []
+    try:
+        for title_num in range(1 ,21):
+            driver.get(url)
+            time.sleep(0.5)
+            print('debug01')
+            movie_title_xpath = '//*[@id="old_content"]/ul/li[{}]/a'.format(title_num)
+            print('debug02')
+            title = driver.find_element('xpath', movie_title_xpath).text
+            print('debug03')
+            driver.find_element('xpath', movie_title_xpath).click()
+            print('debug04')
+            time.sleep(0.1)
+            print('debug05')
+            try:
+                print('debug06')
+                driver.find_element('xpath', review_button_xpath).click()
+                time.sleep(0.1)
+                review_num = driver.find_element('xpath', review_num_path).text
+                print('debug07')
+                review_num = review_num.replace(',', '')
+                review_range = (int(review_num) - 1)// 10 + 1
+                if review_range > 3:
+                    review_range = 3
+
+                for review_page_num in range (1, review_range + 1):
+                    review_page_button_xpath = '// *[ @ id = "pagerTagAnchor{}"]'.format(review_page_num)
+                    driver.find_element('xpath', review_page_button_xpath).click()
+                    time.sleep(0.1)
+
+                    for review_title_num in range(1,11):
+                            review_title_xpath = '//*[@id="reviewTab"]/div/div/ul/li[{}]/a'.format(review_title_num)
+                            driver.find_element('xpath', review_title_xpath).click()
+                            time.sleep(0.1)
+                            try:
+                                review = driver.find_element('xpath', review_xpath).text
+                                titles.append(title)
+                                reviews.append(review)
+                                print(title)
+                                print(review)
+                                driver.back()
+                            except:
+                                print('review',page, title_num, review_title_num)
+                                driver.back()
+            except:
+                print('review button', page, title_num)
+
+
+        df = pd.DataFrame({'titles':titles, 'reviews':reviews})
+        df.to_csv('./crawling_data/reviews_{}_{}page.csv'.format(your_year, page), index=False)
+
+    except:
+        print('error', page, title_num)
 
